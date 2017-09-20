00519e8c8518a6cae96c36259b7a66edfa189fd9
William Jing <william_jing@qq.com>
2017-09-20 13:06:16 +0800

    update dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,19 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
+import { MePage } from "../pages/me/me";
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { SharedProvider } from '../providers/shared/shared';
 
 @NgModule({
   declarations: [
     MyApp,
     AboutPage,
-    ContactPage,
+    MePage,
     HomePage,
     TabsPage
   ],
@@ -27,14 +28,15 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     AboutPage,
-    ContactPage,
+    MePage,
     HomePage,
     TabsPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    SharedProvider
   ]
 })
 export class AppModule {}
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 
 import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
-
+import { MePage } from '../me/me';
 @Component({
   templateUrl: 'tabs.html'
 })
@@ -11,7 +10,7 @@ export class TabsPage {
 
   tab1Root = HomePage;
   tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab3Root = MePage;
 
   constructor() {
 
