17dd599e5d07537f4dda028c4bdbeb7d2a09fc5b
williamjing <william_jing@qq.com>
2017-10-08 15:25:51 +0800

    update pipe of Since

--- src/pipes/since/since.ts
+++ src/pipes/since/since.ts
@@ -24,15 +24,15 @@ export class SincePipe implements PipeTransform {
     console.log("Since info:", years + "..." + months + "..." + days + "..." + hours + "..." + minutes);
     // return value.toLowerCase();
     if (years >= 1) {
-      return years + "年前";
+      return "发布于" + years + "年前";
     } else if (months >= 1) {
-      return months + "个月前";
+      return "发布于" + months + "个月前";
     } else if (days >= 1) {
-      return days + "天前";
+      return "发布于" + days + "天前";
     } else if (hours >= 1) {
-      return hours + "小时前";
+      return "发布于" + hours + "小时前";
     } else {
-      return minutes + "分钟前";
+      return "发布于" + minutes + "分钟前";
     }
   }
 }
