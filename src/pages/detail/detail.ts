2da57f2a97c41755a2ae77bd26b51dd3f98c331c
williamjing <william_jing@qq.com>
2017-10-18 23:55:08 +0800

    thumb up function

--- src/pages/detail/detail.ts
+++ src/pages/detail/detail.ts
@@ -17,19 +17,35 @@ import { SharedProvider, Global } from "../../providers/shared/shared";
 export class DetailPage {
   private id: string;
   private topic: any;
+  private accesstoken: string;
+  private offset: number;
   constructor(public navCtrl: NavController, public navParams: NavParams, public sharedProvider: SharedProvider) {
     this.id = navParams.get("id");
   }
   ngOnInit() {
-    this.topic = this.sharedProvider.httpGet(Global.API.getTopic + this.id, null, true)
+    this.accesstoken = '42c72ac2-62ca-4b66-ad45-b5f5593378be';
+    this.topic = this.sharedProvider.httpGet(Global.API.getTopic + this.id, this.accesstoken ? { 'accesstoken': this.accesstoken } : null, true)
       .then(data => {
         this.topic = data.data;
-      }
-      );
+      });
     console.log("topic:" + this.topic);
   }
 
-  like(reply){
+  like(reply) {
     console.log("like++");
+    this.sharedProvider.httpPost(Global.API.upReply.replace(':reply_id', reply.id), { 'accesstoken': this.accesstoken }, false)
+      .then(data => {
+        if (data.success) {
+          if (data.action == "down") {
+            reply.is_uped = false;
+            reply.ups.pop();
+          } else {
+            reply.is_uped = true;
+            reply.ups.push('');
+          }
+        } else {
+
+        }
+      });
   }
 }
