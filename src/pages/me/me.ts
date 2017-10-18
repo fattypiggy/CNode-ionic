f8fa1fc6a512aa3abcf5a683eafa9fd9c1d1da0a
williamjing <william_jing@qq.com>
2017-10-18 23:56:33 +0800

    add login function

--- src/pages/me/me.ts
+++ src/pages/me/me.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { SharedProvider, Global } from "../../providers/shared/shared";
+
 import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 /**
  * Generated class for the MePage page.
@@ -16,22 +18,29 @@ import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 })
 export class MePage {
 
+  private loginUser: string;
   private accesstoken: string;
-  constructor(public navCtrl: NavController, public navParams: NavParams, public barcodeScanner: BarcodeScanner) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public barcodeScanner: BarcodeScanner, public sharedProvider: SharedProvider) {
   }
 
   scan() {
     this.barcodeScanner.scan().then((barcodeData) => {
       this.accesstoken = barcodeData.text;
       console.log(barcodeData);
+      this.verify(this.accesstoken);
       // Success! Barcode data is here
     }, (err) => {
       console.log(err);
       // An error occurred
     });
   }
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad MePage');
+  verify(accesstoken: string) {
+    this.sharedProvider.httpPost(Global.API.verifyToken, { "accesstoken": accesstoken }, true)
+      .then(data => {
+        this.loginUser = data.loginname;
+        if(this.loginUser){
+          localStorage.setItem('accesstoken',this.accesstoken);
+        }
+      })
   }
-
 }
