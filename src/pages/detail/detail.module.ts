471c5c504656539e4965732f41456a365dd3bd58
williamjing <william_jing@qq.com>
2018-02-21 17:10:32 +0800

    update detail page

--- src/pages/detail/detail.module.ts
+++ src/pages/detail/detail.module.ts
@@ -3,9 +3,12 @@ import { IonicPageModule } from 'ionic-angular';
 import { DetailPage } from './detail';
 
 import { PipesModule } from "../../pipes/pipes.module";
+
+import { KeyboardAttachDirective } from "../../directive/keyboard-attach.directive";
 @NgModule({
   declarations: [
     DetailPage,
+    KeyboardAttachDirective,
   ],
   imports: [
     IonicPageModule.forChild(DetailPage),
--- src/pages/detail/detail.ts
+++ src/pages/detail/detail.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Keyboard } from '@ionic-native/keyboard';
 import { SharedProvider, Global } from "../../providers/shared/shared";
+import { Subscription } from 'rxjs/Subscription';
+
 /**
  * Generated class for the DetailPage page.
  *
@@ -15,9 +17,12 @@ import { SharedProvider, Global } from "../../providers/shared/shared";
   templateUrl: 'detail.html',
 })
 export class DetailPage {
+  private onShowSubscription: Subscription;
+  private onHideSubscription: Subscription;
   private id: string;
   private topic: any;
   private accesstoken: string;
+  private showInput: boolean;
   constructor(public navCtrl: NavController, public navParams: NavParams, public sharedProvider: SharedProvider, private keyboard: Keyboard) {
     this.id = navParams.get("id");
   }
@@ -27,9 +32,14 @@ export class DetailPage {
       .then(data => {
         this.topic = data.data;
       });
+    this.onShowSubscription = this.keyboard.onKeyboardShow().subscribe(e => this.onShow(e));
+    this.onHideSubscription = this.keyboard.onKeyboardHide().subscribe(() => this.onHide());
     console.log("topic:" + this.topic);
   }
 
+  doReply(e) {
+    console.log(e);
+  }
   like(reply) {
     console.log("like++");
     this.sharedProvider.httpPost(Global.API.upReply.replace(':reply_id', reply.id), this.accesstoken ? { 'accesstoken': this.accesstoken } : null, false)
@@ -55,5 +65,16 @@ export class DetailPage {
 
   cancelReply() {
     this.keyboard.close();
+  }
+
+  onShow(e) {
+    this.showInput = true;
+  }
+  onHide() {
+    this.showInput = false;
+  }
+  ngOnDestroy() {
+    if (this.onShowSubscription) this.onShowSubscription.unsubscribe();
+    if (this.onHideSubscription) this.onHideSubscription.unsubscribe();
   }
 }
